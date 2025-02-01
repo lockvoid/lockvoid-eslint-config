ce283f84fc27ad5009da64caa9bdce1f4dd82970
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-02-01 10:01:52 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  {
+    plugins: {
+      import: importPlugin,
+    },
+
+    rules: {
+      'comma-dangle': ['error', 'always-multiline'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+
+      'import/order': [
+        'error',
+
+        {
+          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'alphabetize': { 'order': 'asc', 'caseInsensitive': true },
+        },
+      ],
+    },
+  },
+];
