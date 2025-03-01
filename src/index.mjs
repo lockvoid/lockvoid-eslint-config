ee2b344c417ce2d8b9d815b47350422ba9e7cf32
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-03-01 17:16:10 +0200

    Order #import before internal

--- src/index.mjs
+++ src/index.mjs
@@ -8,13 +8,33 @@ export default {
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'quotes': ['error', 'single', { 'avoidEscape': true }],
+    'semi': ['error', 'always'],
 
     'import/order': [
       'error',
 
       {
-        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        'alphabetize': { 'order': 'asc', 'caseInsensitive': true },
+        'groups': [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'.
+        ],
+
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': true,
+        },
+
+        'pathGroups': [
+          {
+            'group': 'internal',
+            'pattern': '#some-path/**',
+            'position': 'before',
+          },
+        ],
       },
     ],
   },
